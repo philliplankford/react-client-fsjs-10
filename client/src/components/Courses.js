f86ee0a72764bfbca24d0118bddc8e80b9413ecb
Phillip <Flip.dlankford@gmail.com>
2022-01-24 17:40:12 -0800

    create course button

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -18,8 +18,7 @@ export default class Courses extends Component {
                 this.setState({
                     allCourses: response.data,
                 });
-            })
-        
+            }) 
     }
 
     render () {
@@ -37,12 +36,16 @@ export default class Courses extends Component {
                             <h3 className="course--title">{course.title}</h3>   
                         </Link>
                     ))}
-                    <Link 
-                        className="course--module course--link"
-                        to={`/courses`}
+                    <Link
+                        className="course--module course--add--module"
+                        to="/courses/create"
                     >
-                        <h2 className="course--label">Course</h2>
-                        <h3 className="course--title">Course Title</h3>   
+                        <span className="course--add--title">
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
+                            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                            </svg>
+                            New Course
+                        </span>
                     </Link>
                 </div>
             </React.Fragment>
