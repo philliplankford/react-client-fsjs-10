6fdecfdd29055d4e3020ca8c9425f02818fd8e13
Phillip <Flip.dlankford@gmail.com>
2022-01-26 13:14:06 -0800

    converted SignIn class form to use hooks

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,98 +1,66 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withNavigate } from '../Router';
+import React, { useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Form from './Form';
 
-export default class UserSignIn extends Component {
+export default function UserSignIn({ context }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
-    state = {
-        username: '',
-        password: '',
-        errors: [],
-    }
+    const navigate = useNavigate();
 
-    render () {
-
-        const {
-            username,
-            password,
-            errors,
-        } = this.state;
-
-        return (
-            <React.Fragment>
-                <div className="form--centered">
-                    <h2>Sign In</h2>
-                    <Form
-                        cancel={this.cancel}
-                        errors={errors}
-                        submit={this.submit}
-                        submitButtonText="Sign In"
-                        elements={() => (
-                            <React.Fragment>
-                                <input 
-                                    id="username"
-                                    name="username"
-                                    type="text"
-                                    value={username}
-                                    onChange={this.change}
-                                    placeholder="User Name"
-                                />
-                                <input 
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    value={password}
-                                    onChange={this.change}
-                                    placeholder="Password"
-                                />
-                            </React.Fragment>
-                        )}
-                    />
-
-                    {/* <form>
-                        <label for="emailAddress">Email Address</label>
-                        <input id="emailAddress" name="password" type="password" value=""></input>
-                        <label for="password">Password</label>
-                        <input id="password" name="password" type="password" value=""></input>
-                        <button className="button" type="submit">Sign In</button>
-                        <button className="button button-secondary" onClick="event.preventDefault(); location.href='/';"></button>
-                    </form> */}
-                    <p>Don't have a user account? Click <a href="/">here to sign up</a>!</p>
-                </div>
-            </React.Fragment>
-        );
-    }
-
-    change = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        this.setState(() => {
-            return {
-                [name]: value
-            };
-        });
-    }
-
-    submit = () => {
-        const { context } = this.props;
-        const { from } = this.props.location.state || { from: { pathname: '/authenticated'} };
-        const { username, password } = this.state;
-
+    const submit = () => {
         context.actions.signIn(username, password)
             .then( user => {
                 if (user === null) {
-                    this.setState(() => {
-                        return { errors: [`Sign-in was unsuccessful` ]};
-                    });
+                    setErrors([`Sign-in was unsuccessful`]); //setErrors({ errors: [`Sign-in was unsuccessful`]});
                 } else {
-                
+                    navigate(-1); // Equivilent to hitting the back button. Is that what we want?
                 }
             })
+            .catch ( err => {
+                console.log(err);
+                navigate('/error')
+            })
+
     }
 
-    cancel = () => {
-    
+    const cancel = () => {
+        navigate("/");
     }
+
+    return (
+        <React.Fragment>
+            <div className="form--centered">
+                <h2>Sign In</h2>
+                <Form
+                    cancel={cancel}
+                    errors={errors}
+                    submit={submit}
+                    submitButtonText="Sign In"
+                    elements={() => (
+                        <React.Fragment>
+                            <input 
+                                id="username"
+                                name="username"
+                                type="text"
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                                placeholder="User Name"
+                            />
+                            <input 
+                                id="password"
+                                name="password"
+                                type="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                placeholder="Password"
+                            />
+                        </React.Fragment>
+                    )}
+                />
+                <p>Don't have a user account? Click <Link to="/signup">here to sign up</Link>!</p>
+            </div>
+        </React.Fragment>
+    );
 }
